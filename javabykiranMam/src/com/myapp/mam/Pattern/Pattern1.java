4e13ef5e16d0897ad48d0272d27333f3d77be70c
amit <amit@gmail.com>
2021-08-24 10:52:12 -0700

    Token 02 find factorial number og given number done

--- javabykiranMam/src/com/myapp/mam/Pattern/Pattern1.java
+++ javabykiranMam/src/com/myapp/mam/Pattern/Pattern1.java
@@ -1,8 +1,26 @@
 package com.myapp.mam.Pattern;
 
+import java.util.Scanner;
+
 public class Pattern1 {
 public static void main(String[] args) {
 	
-	System.out.println("Commit");
+	System.out.println("find fictorial number");
+	
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.println("enter the number of finding factorial number");
+	
+	int no=sc.nextInt();
+	
+	for(int i=2;i<no;i++)
+	{
+		if(no%i==0)
+		{
+			System.out.println("the factorial number is : "+i);
+			
+		}
+		
+	}
 }
 }
